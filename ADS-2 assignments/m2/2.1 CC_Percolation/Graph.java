838fd3ed9b7ca86e5b23112a0ff699f767e88a0d
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-30 20:14:31 +0530

    "ads2_m2_a1_1: 0/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m2/2.1 CC_Percolation/Graph.java
+++ ADS-2 assignments/m2/2.1 CC_Percolation/Graph.java
@@ -1,98 +1,95 @@
-import java.util.NoSuchElementException;
-public class Graph {
-    private final int V;
-    private int E;
+/**
+ * List of graphs.
+ */
+class Graph {
+    /**
+     * vertices.
+     */
+    private int vertices;
+    /**
+     * edges.
+     */
+    private int edge;
+    /**
+     * bag.
+     */
     private Bag<Integer>[] adj;
-
     /**
-     * Initializes an empty graph with {@code V} vertices and 0 edges.
-     * param V the number of vertices
+     * Constructs the object.
      *
-     * @param  V number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
+     * @param      v1    The v 1
      */
-    public Graph(int V) {
-        if (V < 0) {
-            throw new IllegalArgumentException(
-                "Number of vertices must be nonnegative");
-        }
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
+    Graph(final int v1) {
+        this.vertices = v1;
+        this.edge = 0;
+        this.adj = (Bag<Integer>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Integer>();
         }
     }
-
     /**
-     * Returns the number of vertices in this graph.
+     * vertices.
      *
-     * @return the number of vertices in this graph
+     * @return     { description_of_the_return_value }
      */
-    public int V() {
-        return V;
+    public int vertices() {
+        return this.vertices;
     }
-
     /**
-     * Returns the number of edges in this graph.
+     * number of edges.
      *
-     * @return the number of edges in this graph
+     * @return     { description_of_the_return_value }
      */
-    public int E() {
-        return E;
+    public int edges() {
+        return this.edge;
     }
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException(
-                "vertex " + v + " is not between 0 and " + (V - 1));
-    }
-
     /**
-     * Adds the undirected edge v-w to this graph.
+     * Adds an edge.
      *
-     * @param  v one vertex in the edge
-     * @param  w the other vertex in the edge
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
      */
-    public void addEdge(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        E++;
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w) && v != w) {
+            edge++;
+        }
         adj[v].add(w);
         adj[w].add(v);
     }
-
-
     /**
-     * Returns the vertices adjacent to vertex {@code v}.
+     * iterable.
      *
-     * @param  v the vertex
-     * @return the vertices adjacent to vertex {@code v}, as an iterable
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
-    public Iterable<Integer> adj(int v) {
-        validateVertex(v);
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
-
     /**
-     * Returns the degree of vertex {@code v}.
+     * Determines if it has edge.
      *
-     * @param  v the vertex
-     * @return the degree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
      */
-    public int degree(int v) {
-        validateVertex(v);
-        return adj[v].size();
-    }
-    public boolean hasEdge(int v, int w) {
-        for (int each : adj[w]) {
-            if (each == v) {
-                return true;
+    public boolean hasEdge(final int v, final int w) {
+        int count = 0;
+        for (int i : adj[v]) {
+            if (i == w) {
+                count += 1;
+                break;
             }
+        }
+        for (int i : adj[w]) {
+            if (i == v) {
+                count += 1;
+                break;
+            }
+        }
+        if (count == 2) {
+            return true;
         }
         return false;
     }
