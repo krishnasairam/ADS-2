c8b4faed93ec73c38f26698084122fdfc5a42c34
krishnasairam <sairamadabala007@msitprogram.net>
2018-11-06 22:55:38 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2 assignments/m8/1. Shortest Paths/Solution.java
@@ -14,7 +14,7 @@ public final class Solution {
   private int getIndex(String str) {
     int index = 0;
     for (int i = 0; i < root.length; i++) {
-      if (str == root[i]) {
+      if (str.equals(root[i])) {
         index = i;
       }
     }
@@ -30,31 +30,31 @@ public final class Solution {
   public static void main(final String[] args) {
     Solution sol = new Solution();
     Scanner scan = new Scanner(System.in);
-    int vertices = scan.nextInt();
-    int edges = scan.nextInt();
-    scan.nextLine();
+    String[] values = scan.nextLine().split(" ");
+    int vertices = Integer.parseInt(values[0]);
+    int edges = Integer.parseInt(values[1]);
     String[] tokens = scan.nextLine().split(" ");
-    for(int j = 0; j<tokens.length; j++){
+    sol.root = new String[tokens.length];
+    for (int j = 0; j < tokens.length; j++) {
       sol.root[j] = tokens[j];
     }
-    EdgeWeightedDigraph eg = new EdgeWeightedDigraph(vertices);
+    EdgeWeightedGraph eg = new EdgeWeightedGraph(vertices);
     while (edges > 0) {
       String[] token = scan.nextLine().split(" ");
       int first = sol.getIndex(token[0]);
       int second = sol.getIndex(token[1]);
       int weight = Integer.parseInt(token[2]);
-      DirectedEdge e = new DirectedEdge(first, second, weight);
+      Edge e = new Edge(first, second, weight);
       eg.addEdge(e);
-      System.out.println(eg);
       edges--;
     }
     int i = Integer.parseInt(scan.nextLine());
     while (i > 0) {
       String[] tokens1 = scan.nextLine().split(" ");
       int home = sol.getIndex(tokens1[0]);
-      DijkstraSP l = new DijkstraSP(eg, home);
+      SP l = new SP(eg, home);
       int destination = sol.getIndex(tokens1[1]);
-      System.out.printf("%f\n", l.distTo(destination));
+      System.out.printf("%d\n", (int) l.distTo(destination));
       i--;
     }
   }
