1a533f5fa74b4dfe4d2f851492342778db893f24
krishnasairam <sairamadabala007@msitprogram.net>
2018-11-09 11:07:11 +0530

    "ads2_m10_a1_1: 4/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2 assignments/m10/Seam Carving/SeamCarver.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.lang.Math;
+import java.util.Arrays;
 public class SeamCarver {
 	/**
 	 * picture.
@@ -17,6 +18,17 @@ public class SeamCarver {
 	 * energy 2d array.
 	 */
 	private double energy[][];
+	/**
+	 * distTO
+	 */
+	private double[][] distTo;
+	/**
+	 * distToSink.
+	 */
+    private double distToSink;
+    private int[][] edgeTo;
+    private int edgeToSink;
+    private boolean transposed;
 	/**
 	 * Constructs the object.
 	 *
@@ -79,17 +91,64 @@ public class SeamCarver {
 	public double energy(int x, int y) {
 		return energy[x][y];
 	}
-
 	// sequence of indices for horizontal seam
 	public int[] findHorizontalSeam() {
 		return new int[0];
 	}
-
 	// sequence of indices for vertical seam
 	public int[] findVerticalSeam() {
-		return new int[0];
-	}
+		transposed = false;
+        distToSink = Double.POSITIVE_INFINITY;
+        edgeToSink = Integer.MAX_VALUE;
+        distTo = new double[width][height];
+        edgeTo = new int[width][height];
+        for (double[] r: distTo) {
+        	Arrays.fill(r, Double.POSITIVE_INFINITY);
+        }
+        for (int[] r: edgeTo) {
+        	Arrays.fill(r, Integer.MAX_VALUE);
+        }
 
+        // Relax the entire top row, since this is our starting row
+        Arrays.fill(distTo[0], (double) 1000);
+        Arrays.fill(edgeTo[0], -1);
+
+        // Visit all pixels from the top, diagonally to the right,
+        // in keeping with topological order.
+        // The topological order is the reverse of the DFS post-order,
+        // which visits the left-most adjacent pixel first, before it visits
+        // pixels to the right.
+        for (int top = width() - 1; top >= 0; top--) {
+            for (int depth = 0;
+                    depth + top < width() && depth < height();
+                    depth++) {
+                visit(depth, depth + top);
+            }
+        }
+        // Visit all pixels from the left side, diagonally to the right,
+        // in keeping with the topological order described above.
+        for (int depth = 1; depth < height(); depth++) {
+            for (int out = 0;
+                    out < width() && depth + out < height();
+                    out++) {
+                visit(depth + out, out);
+            }
+        }
+
+        // Populate seam[] with the shortest path
+        int[] seam = new int[height()];
+        seam[height() - 1] = edgeToSink;
+
+        for (int i = height() - 1; i > 0; i--) {
+            seam[i - 1] = edgeTo[i][seam[i]];
+        }
+
+        // null out our shortest-path arrays for garbage collection
+        distTo = null;
+        edgeTo = null;
+
+        return seam;
+    }
 	// remove horizontal seam from current picture
 	public void removeHorizontalSeam(int[] seam) {
 
@@ -99,4 +158,90 @@ public class SeamCarver {
 	public void removeVerticalSeam(int[] seam) {
 
 	}
+	private void visit(int i, int j) {
+        if (transposed) {
+            // Only relax the sink
+            if (j == width() - 1) {
+                relax(i, j);
+            }
+
+            // Bottom edge; relax to the right and above
+            else if (i == height() - 1) {
+                relax(i, j, i, j + 1);
+                relax(i, j, i - 1, j + 1);
+            }
+
+            // Top edge; relax to the right and below
+            else if (i == 0) {
+                relax(i, j, i, j + 1);
+                relax(i, j, i + 1, j + 1);
+            }
+
+            // Middle pixel; relax right, below, and above
+            else {
+                relax(i, j, i - 1, j + 1);
+                relax(i, j, i, j + 1);
+                relax(i, j, i + 1, j + 1);
+            }
+        }
+
+        else {
+            // Only relax the sink
+            if (i == height() - 1) {
+                relax(i, j);
+            }
+
+            // Right edge; relax below and to the left
+            else if (j == width() - 1) {
+                relax(i, j, i + 1, j - 1);
+                relax(i, j, i + 1, j);
+            }
+
+            // Left edge; relax below and to the right
+            else if (j == 0) {
+                relax(i, j, i + 1, j);
+                relax(i, j, i + 1, j + 1);
+            }
+
+            // Middle pixel; relax left, below, and right
+            else {
+                relax(i, j, i + 1, j - 1);
+                relax(i, j, i + 1, j);
+                relax(i, j, i + 1, j + 1);
+            }
+        }
+    }
+
+    /**
+     * Given an index, relax the sink vertex from that index.
+     *
+     * This method should only be called on the "last" vertices in the image.
+     *
+     * @param i the vertical index of the pixel
+     * @param j the horizontal index of the pixel
+     */
+    private void relax(int i, int j) {
+        //if (validIndex(i, j)) {
+            if (distToSink > distTo[i][j]) {
+                distToSink = distTo[i][j];
+                if (transposed) {
+                	edgeToSink = i;
+                } else {
+                	edgeToSink = j;
+                }
+            }
+        //}
+    }
+    private void relax(int i1, int j1, int i2, int j2) {
+        //if (validIndex(i1, j1) && validIndex(i2, j2)) {
+            if (distTo[i2][j2] > distTo[i1][j1] + energy[i2][j2]) {
+                distTo[i2][j2] = distTo[i1][j1] + energy[i2][j2];
+                if (transposed) {
+                	edgeTo[i2][j2] = i1;
+                } else {
+                	edgeTo[i2][j2] = j1;
+                }
+            }
+        //}
+    }
 }
