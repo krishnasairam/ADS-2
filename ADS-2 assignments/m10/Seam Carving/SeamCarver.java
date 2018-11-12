df2328b5c6995b8217bdc445f6ab4cfb62bba932
krishnasairam <sairamadabala007@msitprogram.net>
2018-11-12 09:10:26 +0530

    "ads2_m10_a1_1: 4/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2 assignments/m10/Seam Carving/SeamCarver.java
@@ -2,111 +2,111 @@ import java.awt.Color;
 import java.lang.Math;
 import java.util.Arrays;
 public class SeamCarver {
-	/**
-	 * picture.
-	 */
-	private Picture picture;
-	/**
-	 * width.
-	 */
-	private int width;
-	/**
-	 * height.
-	 */
-	private int height;
-	/**
-	 * energy 2d array.
-	 */
-	private double energy[][];
-	/**
-	 * distTO
-	 */
-	private double[][] distTo;
-	/**
-	 * distToSink.
-	 */
+    /**
+     * picture.
+     */
+    private Picture picture;
+    /**
+     * width.
+     */
+    private int width;
+    /**
+     * height.
+     */
+    private int height;
+    /**
+     * energy 2d array.
+     */
+    private double energy[][];
+    /**
+     * distTO
+     */
+    private double[][] distTo;
+    /**
+     * distToSink.
+     */
     private double distToSink;
     private int[][] edgeTo;
     private int edgeToSink;
     private boolean transposed;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      picture  The picture
-	 */
-	public SeamCarver(Picture picture) {
-		this.picture = picture;
-		this.width = picture.width();
-		this.height = picture.height();
-		this.energy = new double[width][height];
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height ; j++) {
-				if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
-					energy[i][j] = 1000.0;
-				} else {
-					energy[i][j] = gradient(i, j);
-				}
-			}
-		}
-	}
-	public double gradient(int i, int j) {
-		int x_gradient = x_gradient(i, j);
-		int y_gradient = y_gradient(i, j);
-		double en = Math.sqrt(x_gradient + y_gradient);
-		return en;
-	}
-	public int x_gradient(int i, int j) {
-		Color color1 = picture.get(i - 1, j);
-		Color color2 = picture.get(i + 1, j);
-		int red = color1.getRed() - color2.getRed();
-		int green = color1.getGreen() - color2.getGreen();
-		int blue = color1.getBlue() - color2.getBlue();
-		int x_gradient = (red * red) + (green * green) + (blue * blue);
-		return x_gradient;
-	}
-	public int y_gradient(int i, int j) {
-		Color color1 = picture.get(i, j - 1);
-		Color color2 = picture.get(i, j + 1);
-		int red = color1.getRed() - color2.getRed();
-		int green = color1.getGreen() - color2.getGreen();
-		int blue = color1.getBlue() - color2.getBlue();
-		int y_gradient = (red * red) + (green * green) + (blue * blue);
-		return y_gradient;
-	}
-	// current picture
-	public Picture picture() {
-		return picture;
-	}
-	// width of current picture
-	public int width() {
-		return width;
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      picture  The picture
+     */
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
+        this.width = picture.width();
+        this.height = picture.height();
+        this.energy = new double[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height ; j++) {
+                if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
+                    energy[i][j] = 1000.0;
+                } else {
+                    energy[i][j] = gradient(i, j);
+                }
+            }
+        }
+    }
+    public double gradient(int i, int j) {
+        int x_gradient = x_gradient(i, j);
+        int y_gradient = y_gradient(i, j);
+        double en = Math.sqrt(x_gradient + y_gradient);
+        return en;
+    }
+    public int x_gradient(int i, int j) {
+        Color color1 = picture.get(i - 1, j);
+        Color color2 = picture.get(i + 1, j);
+        int red = color1.getRed() - color2.getRed();
+        int green = color1.getGreen() - color2.getGreen();
+        int blue = color1.getBlue() - color2.getBlue();
+        int x_gradient = (red * red) + (green * green) + (blue * blue);
+        return x_gradient;
+    }
+    public int y_gradient(int i, int j) {
+        Color color1 = picture.get(i, j - 1);
+        Color color2 = picture.get(i, j + 1);
+        int red = color1.getRed() - color2.getRed();
+        int green = color1.getGreen() - color2.getGreen();
+        int blue = color1.getBlue() - color2.getBlue();
+        int y_gradient = (red * red) + (green * green) + (blue * blue);
+        return y_gradient;
+    }
+    // current picture
+    public Picture picture() {
+        return picture;
+    }
+    // width of current picture
+    public int width() {
+        return width;
+    }
 
-	// height of current picture
-	public int height() {
-		return height;
-	}
+    // height of current picture
+    public int height() {
+        return height;
+    }
 
-	// energy of pixel at column x and row y
-	public double energy(int x, int y) {
-		return energy[x][y];
-	}
-	// sequence of indices for horizontal seam
-	public int[] findHorizontalSeam() {
-		return new int[0];
-	}
-	// sequence of indices for vertical seam
-	public int[] findVerticalSeam() {
-		transposed = false;
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return energy[x][y];
+    }
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[0];
+    }
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        transposed = false;
         distToSink = Double.POSITIVE_INFINITY;
         edgeToSink = Integer.MAX_VALUE;
         distTo = new double[width][height];
         edgeTo = new int[width][height];
-        for (double[] r: distTo) {
-        	Arrays.fill(r, Double.POSITIVE_INFINITY);
+        for (double[] r : distTo) {
+            Arrays.fill(r, Double.POSITIVE_INFINITY);
         }
-        for (int[] r: edgeTo) {
-        	Arrays.fill(r, Integer.MAX_VALUE);
+        for (int[] r : edgeTo) {
+            Arrays.fill(r, Integer.MAX_VALUE);
         }
 
         // Relax the entire top row, since this is our starting row
@@ -149,16 +149,16 @@ public class SeamCarver {
 
         return seam;
     }
-	// remove horizontal seam from current picture
-	public void removeHorizontalSeam(int[] seam) {
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
 
-	}
+    }
 
-	// remove vertical seam from current picture
-	public void removeVerticalSeam(int[] seam) {
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
 
-	}
-	private void visit(int i, int j) {
+    }
+    private void visit(int i, int j) {
         if (transposed) {
             // Only relax the sink
             if (j == width() - 1) {
@@ -221,27 +221,24 @@ public class SeamCarver {
      * @param j the horizontal index of the pixel
      */
     private void relax(int i, int j) {
-        //if (validIndex(i, j)) {
-            if (distToSink > distTo[i][j]) {
-                distToSink = distTo[i][j];
-                if (transposed) {
-                	edgeToSink = i;
-                } else {
-                	edgeToSink = j;
-                }
+        if (distToSink > distTo[i][j]) {
+            distToSink = distTo[i][j];
+            if (transposed) {
+                edgeToSink = i;
+            } else {
+                edgeToSink = j;
             }
+        }
         //}
     }
     private void relax(int i1, int j1, int i2, int j2) {
-        //if (validIndex(i1, j1) && validIndex(i2, j2)) {
-            if (distTo[i2][j2] > distTo[i1][j1] + energy[i2][j2]) {
-                distTo[i2][j2] = distTo[i1][j1] + energy[i2][j2];
-                if (transposed) {
-                	edgeTo[i2][j2] = i1;
-                } else {
-                	edgeTo[i2][j2] = j1;
-                }
+        if (distTo[i2][j2] > distTo[i1][j1] + energy[i2][j2]) {
+            distTo[i2][j2] = distTo[i1][j1] + energy[i2][j2];
+            if (transposed) {
+                edgeTo[i2][j2] = i1;
+            } else {
+                edgeTo[i2][j2] = j1;
             }
-        //}
+        }
     }
 }
