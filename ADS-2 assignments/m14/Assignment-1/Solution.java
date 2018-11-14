69edfb9d04880018c7fbce33c402cdcd716ad548
krishnasairam <sairamadabala007@msitprogram.net>
2018-11-14 14:00:33 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m14/Assignment-1/Solution.java
+++ ADS-2 assignments/m14/Assignment-1/Solution.java
@@ -15,13 +15,14 @@ public final class Solution {
   }
   /**
    * Main Function.
-   *
+   * time complexity is O(n)
    * @param      args  The arguments
    */
   public static void main(final String[] args) {
     Scanner sc = new Scanner(System.in);
     String prefix = sc.nextLine();
-    String[] words = loadWords();
+    In in = new In("/Files/dictionary-algs4.txt");
+    String[] words = in.readAllStrings();
     TST symbolTable = new TST();
     for (int i = 0; i < words.length; i++) {
       for (int j = 0; j < words[i].length(); j++) {
@@ -33,16 +34,7 @@ public final class Solution {
       System.out.println(s);
     }
   }
-  /**
-   * Loads words.
-   *
-   * @return     { description_of_the_return_value }
-   */
-  public static String[] loadWords() {
-    In in = new In("/Files/dictionary-algs4.txt");
-    String[] words = in.readAllStrings();
-    return words;
-  }
 }
+
 
 
